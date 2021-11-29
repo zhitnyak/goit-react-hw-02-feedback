c6d0892021cd33b4b771cb6bf2835bc2a54e2738
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-11-29 22:27:41 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Vidget from './components/Vidget';
-import TodoList from './components/TodoList/TodoList';
-import initialTodos from './components/TodoList/todos.json';
+// import TodoList from './components/TodoList/TodoList';
+// import initialTodos from './components/TodoList/todos.json';
 
 class App extends Component {
   state = {
-    todos: initialTodos,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
-  deleteTodo = todoId => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== todoId),
-    }));
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
   };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const res = Math.round((good / this.countTotalFeedback()) * 100);
+    if (!res) {
+      return 0;
+    }
+    return res;
+  };
+
+  handleButtonClick = e => {
+    const key = e.target.id;
+    this.setState(prevState => {
+      return { [key]: prevState[key] + 1 };
+    });
+  };
+
   render() {
-    const { todos } = this.state;
-    const completedTodo = todos.reduce(
-      (acc, todo) => (todo.completed ? acc + 1 : acc),
-      0,
-    );
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+
     return (
-      <>
-        <div>
-          <span>ОБЩЕЕ Кол-во: {todos.length}</span>
-          <span>Выполнeнных: {completedTodo}</span>
-        </div>
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-      </>
+      <div className="App">
+        {/* <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleButtonClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section> */}
+      </div>
     );
   }
 }
+// class App extends Component {
+//   state = {
+//     todos: initialTodos,
+//   };
+
+//   deleteTodo = todoId => {
+//     this.setState(prevState => ({
+//       todos: prevState.todos.filter(todo => todo.id !== todoId),
+//     }));
+//   };
+//   render() {
+//     const { todos } = this.state;
+//     const completedTodo = todos.reduce(
+//       (acc, todo) => (todo.completed ? acc + 1 : acc),
+//       0,
+//     );
+//     return (
+//       <>
+//         <div>
+//           <span>ОБЩЕЕ Кол-во: {todos.length}</span>
+//           <span>Выполнeнных: {completedTodo}</span>
+//         </div>
+//         <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+//       </>
+//     );
+//   }
+// }
 
 export default App;
