fc57c311808a4af0bddf4c6ca27f05bdcf8c971b
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-11-30 23:58:47 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -55,32 +55,5 @@ class App extends Component {
     );
   }
 }
-// class App extends Component {
-//   state = {
-//     todos: initialTodos,
-//   };
-
-//   deleteTodo = todoId => {
-//     this.setState(prevState => ({
-//       todos: prevState.todos.filter(todo => todo.id !== todoId),
-//     }));
-//   };
-//   render() {
-//     const { todos } = this.state;
-//     const completedTodo = todos.reduce(
-//       (acc, todo) => (todo.completed ? acc + 1 : acc),
-//       0,
-//     );
-//     return (
-//       <>
-//         <div>
-//           <span>ОБЩЕЕ Кол-во: {todos.length}</span>
-//           <span>Выполнeнных: {completedTodo}</span>
-//         </div>
-//         <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-//       </>
-//     );
-//   }
-// }
 
 export default App;
