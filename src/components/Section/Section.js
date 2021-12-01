10e6e30ecf98b6d234497e84088c860fc56822aa
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-01 13:19:27 +0200

    fix css

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import { Fragment } from 'react';
+import css from './Section.module.css';
 
 const Section = ({ title, children }) => (
-  <>
+  <section className={css.box}>
     <h1>{title}</h1>
     {children}
-  </>
+  </section>
 );
 Section.propTypes = {
   title: PropTypes.string.isRequired,
